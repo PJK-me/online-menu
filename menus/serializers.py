bfee338dfffe0588a96c3727e816dc19f458b37d
PJK-me <geekarus.it@gmail.com>
2021-03-08 01:22:42 +0100

    Added 'is_vegetarian' to DishSerializer

--- menus/serializers.py
+++ menus/serializers.py
@@ -1,21 +1,25 @@
 from rest_framework import serializers
 from menus.models import Menu, Dish
 
+
 class MenuSerializer(serializers.ModelSerializer):
     class Meta:
         model = Menu
         fields = ('id', 'name', 'description', 'created_date', 'updated_date')
         read_only_fields = ('created_date', 'updated_date')
 
+
 class MenuIdSerializer(serializers.ModelSerializer):
     class Meta:
         model = Menu
         fields = ('id', )
 
+
 class DishSerializer(serializers.ModelSerializer):
     class Meta:
         model = Dish
-        fields = ('id', 'name', 'description', 'price', 'prepare_time_minutes', 'menu', 'created_date', 'updated_date')
+        fields = ('id', 'name', 'description', 'price', 'prepare_time_minutes', 'menu', 'is_vegetarian',
+                  'created_date', 'updated_date')
         read_only_fields = ('created_date', 'updated_date')
 
     def to_representation(self, instance):
