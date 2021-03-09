b54b88eb537ea910a24787dc04be0e40969d16d5
PJK-me <geekarus.it@gmail.com>
2021-03-09 06:48:38 +0100

    Added tests for Menus, Updated Menu View with previously deleted code

--- menus/tests.py
+++ menus/tests.py
@@ -1,2 +1,67 @@
+from django.contrib.auth import get_user_model
+from django.db.models import Count
+from django.urls import reverse
 from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APIClient
+from menus.models import Menu, Dish
+from menus.serializers import MenuSerializer, MenuDetailSerializer, DishSerializer
+
+
+class MenuApiTest(TestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+        self.user = get_user_model().objects.create_user(
+            'test',
+            'passpass123'
+        )
+
+        Menu.objects.create(name='Main', description="main desc")
+        Menu.objects.create(name='Other', description="other desc")
+        Menu.objects.create(name='Next', description="next desc")
+
+    def test_retrieve_menu_list_unauthorized(self):
+        response = self.client.get("/api/v1/menu/")
+        ingredients = Menu.objects.all().annotate(dish_count=Count('dish')).filter(dish_count__gt=0).order_by("-id")
+        serializer = MenuSerializer(ingredients, many=True)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, serializer.data)
+
+    def test_retrieve_menu_list_authorized(self):
+        self.client.force_authenticate(self.user)
+        response = self.client.get("/api/v1/menu/")
+        ingredients = Menu.objects.all().annotate(dish_count=Count('dish'))
+        serializer = MenuSerializer(ingredients, many=True)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, serializer.data)
+
+    def test_create_menu_unauthorized(self):
+        body = {
+            'name': 'Another',
+            "description": "another menu"
+        }
+
+        response = self.client.post("/api/v1/menu/", body)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        exists = Menu.objects.filter(
+            name=body['name'],
+            description=body['description'],
+        ).exists()
+        self.assertFalse(exists)
+
+    def test_create_menu_authorized(self):
+        self.client.force_authenticate(self.user)
+        body = {
+            'name': 'Another',
+            "description": "another menu"
+        }
+
+        response = self.client.post("/api/v1/menu/", body)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        exists = Menu.objects.filter(
+            name=body['name'],
+            description=body['description'],
+        ).exists()
+        self.assertTrue(exists)
 
--- menus/views.py
+++ menus/views.py
@@ -1,8 +1,15 @@
 from django.db.models import Count
+from django.http import Http404
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
+from rest_framework.generics import get_object_or_404
+from rest_framework.mixins import ListModelMixin
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 from menus.models import Menu, Dish
 from menus.serializers import MenuSerializer, DishSerializer, MenuDetailSerializer
-from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated, AllowAny
 from rest_framework import filters
 
 
@@ -10,13 +17,23 @@ class MenuViewset(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Menu.objects.all().annotate(dish_count=Count('dish'))
     serializer_class = MenuDetailSerializer
-    filter_backends = [filters.OrderingFilter]
+    filter_backends = [filters.OrderingFilter, DjangoFilterBackend]
     ordering_fields = ['name', 'dish_count']
+    filterset_fields = {
+        'name': ['exact', 'contains'],
+        'created_date': ['exact', 'lte', 'gte'],
+        'updated_date': ['exact', 'lte', 'gte'],
+    }
 
     def get_serializer_class(self):
         if self.action == 'retrieve':
             return MenuDetailSerializer
         return MenuSerializer
+
+    def get_queryset(self):
+        if not self.request.user.is_authenticated:
+            self.queryset = self.queryset.filter(dish_count__gt=0)
+        return self.queryset
 
 
 class DishViewset(viewsets.ModelViewSet):
